3fab84ca8ce8d174d5448acbbdd33db51b4bc1a7
Nikhilesh Gurjar <nikhilesh.gurjar@openxcell.com>
2024-05-01 23:29:25 +0530

    UI and API integration

--- src/App.js
+++ src/App.js
@@ -1,8 +1,52 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import SearchBox from "./components/SearchBox/SearchBox";
+import Table from "./components/Table/Table";
+import fetchDataFromAPI from "./Services/api";
 
 function App() {
+  const [searchValue, setSearchValue] = useState("");
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = async (searchQuery) => {
+    setLoading(true);
+    try {
+      const fetchedData = await fetchDataFromAPI("IN", searchQuery, 10);
+      setData(fetchedData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData("");
+  }, []);
+
+  const handleInputChange = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  const handleSearch = () => {
+    fetchData(searchValue.trim());
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  };
+
   return (
     <div className="App">
+      <SearchBox
+        value={searchValue}
+        onChange={handleInputChange}
+        onKeyPress={handleKeyPress}
+        onSearch={handleSearch}
+      />
+      <Table data={data} loading={loading} />
     </div>
   );
 }
